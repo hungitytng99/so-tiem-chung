841e3a792fd9673d957d8c47aeffeec8d2f06a7f
Manh Hung <mhungict99@gmail.com>
2022-07-06 10:18:37 +0700

    Change dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux';
 import App from './App';
@@ -9,12 +9,13 @@ import { I18nextProvider } from 'react-i18next';
 import './index.sass';
 import * as serviceWorker from './serviceWorker';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.render(
     <Provider store={store}>
         <I18nextProvider i18n={i18n}>
             <App />
         </I18nextProvider>
     </Provider>,
+    document.getElementById('root'),
 );
 
 if (process.env.NODE_ENV !== 'development') {
