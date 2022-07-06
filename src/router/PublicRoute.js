e53c6466e76ca5876afe75e4797003839c8b7fa1
Manh Hung <mhungict99@gmail.com>
2022-07-06 10:11:55 +0700

    Disabled route

--- src/router/PublicRoute.js
+++ src/router/PublicRoute.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 function PublicRoute({ component: Component, needCheckToken = true, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                return <Component {...props} />;
+            }}
+        />
+    );
     if (localStorage.getItem(TOKEN_KEY) && needCheckToken) {
         return <Redirect to="/" />;
     } else
